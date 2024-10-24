2f7683d38043cffabff389ed17b11d2229206768
webQbe <webcubesolutionslk@gmail.com>
2024-10-24 21:36:42 +0530

    Defined DOM elements

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// Define DOM Elements
+const time = document.getElementById('time'),
+    greeting = document.getElementById('greeting'),
+    name = document.getElementById('name'),
+    focus = document.getElementById('focus');
